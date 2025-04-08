3be91985050b258bba04e068cbe2a140edff376a
zsoltpete <pete.zsolt@codeyard.eu>
2025-04-08 15:54:40 +0200

    genre section implemented

---
+++ movie-app-live/helpers/Fonts.swift
@@ -0,0 +1,16 @@
+//
+//  Fonts.swift
+//  movie-app-live
+//
+//  Created by Zsolt Pete on 2025. 04. 08..
+//
+
+import SwiftUI
+
+enum Fonts {
+    static let heading = Font.custom("Raleway-ExtraBold", size: 32)
+    static let title = Font.custom("Raleway-ExtraBold", size: 24)
+    static let subheading = Font.custom("Raleway-ExtraBold", size: 16)
+    static let paragraph = Font.custom("Lato-Regular", size: 12)
+    static let caption = Font.custom("Lato-Bold", size: 12)
+}
