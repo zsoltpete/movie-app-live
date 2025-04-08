de119e83e386b3cc17672c84c1d5c7735c293970
zsoltpete <pete.zsolt@codeyard.eu>
2025-04-08 12:58:48 +0200

    new padding added

--- movie-app-live/ContentView.swift
+++ movie-app-live/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
-        .padding()
+        .padding(.horizontal, 10.0)
         .background(.purple)
     }
 }
