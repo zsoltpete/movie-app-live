0f151e9bd315506f4831843083192fc3097ed39a
zsoltpete <pete.zsolt@codeyard.eu>
2025-04-08 12:11:29 +0200

    Initial commit

---
+++ movie-app-live/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movie-app-live
+//
+//  Created by Zsolt Pete on 2025. 04. 08..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movie-app-live/movie_app_liveApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_app_liveApp.swift
+//  movie-app-live
+//
+//  Created by Zsolt Pete on 2025. 04. 08..
+//
+
+import SwiftUI
+
+@main
+struct movie_app_liveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
